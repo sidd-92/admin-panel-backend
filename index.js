1b646315506e3b9c5b79028575e46d6b0a464fcd
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2020-11-25 16:07:02 +0530

    Send Mail and User Auth

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require("dotenv").config(); // Loading dotenv to have access to env variables
 
 const express = require("express");
+const mailgun = require("mailgun-js");
 const morgan = require("morgan");
 const cors = require("cors");
+const checkAuth = require("./middleware/check-auth");
 const app = express();
 const connectDB = require("./config/db");
 
@@ -18,5 +20,28 @@ app.get("/", (req, res, next) => {
   });
 });
 
+app.post("/sendmail", checkAuth, (req, res, next) => {
+  let email = req.body.email;
+  const DOMAIN = process.env.MAILGUN_DOMAIN;
+  const mg = mailgun({ apiKey: process.env.MAILGUN_API, domain: DOMAIN });
+  const data = {
+    from: process.env.MAILGUN_URL,
+    to: "sidddude92@gmail.com",
+    subject: "Hello",
+    text: "Testing some Mailgun awesomness! click on this link to Reset Password https://www.google.com",
+    template: "resetpassword",
+  };
+  mg.messages().send(data, function (error, body) {
+    if (error) {
+      console.log("ERROR", error);
+      res.status(404).json({ message: body });
+    } else {
+      console.log(body);
+      res.status(200).json({ message: `Mail Was Sent to ${email} ` });
+    }
+  });
+});
+
 app.use("/api/posts", require("./routes/api/post"));
+app.use("/api/users", require("./routes/api/user"));
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+    enum: ["user", "admin"],
+  },
+});
+
+module.exports = mongoose.model("user", UserSchema);
